f040db4a5a98d806c22b7f6d5a1333a349c5d5ca
Rafael do Prado <rafa40026@gmail.com>
2025-04-08 21:17:40 -0300

    Add files via upload

---
+++ exercicios-vazios-main/Object/script.js
@@ -0,0 +1,27 @@
+/*var pessoa = {
+    nome: "Rafael",
+    idade: 25,
+    bigode:true,
+    altura: 1.81
+}
+
+alert(pessoa.altura)*/
+
+
+var carro = { 
+    marca:"Ford",
+    modelo:"fusion",
+    ano: 2020,
+    portas: 4,
+    cor:"Branco",
+    preco: 130000
+
+}
+
+alert(`Marca: ${carro.marca} 
+    modelo: ${carro.modelo} 
+    ano: ${carro.ano} 
+    portas: ${carro.portas} 
+    cor: ${carro.cor}
+    preco: ${carro.preco}
+    `)
---
+++ exercicios-vazios-main/array/script.js
@@ -0,0 +1,4 @@
+/*var carros = ["Civic","BMW", "Corsa","Fusca"]
+alert (carros[3])*/
+
+var numeros = [ [1,2,3] , [4,5,6]]
